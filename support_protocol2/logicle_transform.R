a5b4b82c8180ec48d6ce6fd39821e43b7aa7a2cc
Ankur Ankan <ankurankan@gmail.com>
2021-02-08 20:31:40 +0100

    Update code to correct size

--- support_protocol2/logicle_transform.R
+++ support_protocol2/logicle_transform.R
@@ -12,6 +12,8 @@ lgcl <- estimateLogicle(d_flow, channels = colnames( d_flow ) )
 d_trans <- exprs( transform( d_flow, lgcl ) )
 
 # Plot one of the columns
+pdf( "logicle.pdf", width = 7, height = 3.5 )
 par( font.main=1, cex.main=1, bty='l', mfrow=c( 1, 2 ), pch=19, mar=c( 4, 4, 2, 1 ) )
-qqnorm( dataset[, 4], main="Before log transformation", cex=.5 )
-qqnorm( d_trans[, 4], main="After log transformation", cex=.5 )
+qqnorm( dataset[, 4], main="Before logicle transformation", cex=.5 )
+qqnorm( d_trans[, 4], main="After logicle transformation", cex=.5 )
+dev.off()
