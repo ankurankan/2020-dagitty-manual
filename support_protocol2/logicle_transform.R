61d002a361994941e795bcb76fb93336db21683c
Ankur Ankan <ankurankan@gmail.com>
2021-02-08 20:04:20 +0100

    Consistent code style

--- support_protocol2/logicle_transform.R
+++ support_protocol2/logicle_transform.R
@@ -8,7 +8,7 @@ dataset <- as.matrix( read_excel( "cd3cd28.xls" ) )
 
 # Transform the dataset
 d_flow <- as_flowFrame(dataset)
-lgcl <- estimateLogicle(d_flow, channels = colnames(d_flow))
+lgcl <- estimateLogicle(d_flow, channels = colnames( d_flow ) )
 d_trans <- exprs( transform( d_flow, lgcl ) )
 
 # Plot one of the columns
